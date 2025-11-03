9ce1d4f10fec24a3c909aaf6b46c015d7361d9f0
wisemoon <willyedoo@gmail.com>
2025-11-03 17:01:11 +0100

    Refactor drafts table and ensure sequence creation
    Updated the drafts table schema and added checks for sequence creation.

--- sql/Drafts.pg.sql
+++ sql/Drafts.pg.sql
@@ -1,36 +1,39 @@
--- 
--- SQL for Drafts Extension
--- 
--- Table for storing working changes to pages that
--- users have yet to commit.
--- Postgres version
--- See Drafts.sql for notes on each column
+-- SQL for Drafts Extension (PostgreSQL version)
+-- Stores working changes to pages that users have yet to commit
+
 BEGIN;
 
-CREATE SEQUENCE drafts_id_seq;
-
 CREATE TABLE drafts (
-  draft_id INTEGER NOT NULL DEFAULT nextval('drafts_id_seq'),
-  draft_token TEXT,
-  draft_user INTEGER NOT NULL DEFAULT 0,
-  draft_page INTEGER NOT NULL default 0,
-  draft_namespace INTEGER NOT NULL,
-  draft_title TEXT NOT NULL DEFAULT '',
-  draft_section SMALLINT,
-  draft_starttime TIMESTAMPTZ,
-  draft_edittime  TIMESTAMPTZ,
-  draft_savetime  TIMESTAMPTZ,
-  draft_scrolltop INTEGER,
-  draft_text TEXT NOT NULL,
-  draft_summary TEXT,
-  draft_minoredit SMALLINT,
-  PRIMARY KEY (draft_id)
+    draft_id INTEGER PRIMARY KEY DEFAULT nextval('drafts_id_seq'),
+    draft_token TEXT,
+    draft_user INTEGER NOT NULL DEFAULT 0,
+    draft_page INTEGER NOT NULL DEFAULT 0,
+    draft_namespace INTEGER NOT NULL,
+    draft_title TEXT NOT NULL DEFAULT '',
+    draft_section SMALLINT,
+    draft_starttime TIMESTAMPTZ,
+    draft_edittime TIMESTAMPTZ,
+    draft_savetime TIMESTAMPTZ,
+    draft_scrolltop INTEGER,
+    draft_text TEXT NOT NULL,
+    draft_summary TEXT,
+    draft_minoredit BOOLEAN
 );
 
--- Todo: determine which of these are really needed
-CREATE INDEX draft_user_savetime ON drafts(draft_user, draft_savetime);
-CREATE INDEX draft_user_page_savetime ON drafts(draft_user, draft_page, draft_namespace, draft_title, draft_savetime);
-CREATE INDEX draft_savetime ON drafts(draft_savetime);
-CREATE INDEX draft_page ON drafts(draft_page);
+-- Create the sequence for draft_id if not already present
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_class WHERE relname = 'drafts_id_seq') THEN
+        CREATE SEQUENCE drafts_id_seq OWNED BY drafts.draft_id;
+    END IF;
+END
+$$;
+
+-- Indexes to optimize queries
+CREATE INDEX draft_user_savetime ON drafts (draft_user, draft_savetime);
+CREATE INDEX draft_user_page_savetime ON drafts (draft_user, draft_page, draft_namespace, draft_title, draft_savetime);
+CREATE INDEX draft_savetime ON drafts (draft_savetime);
+CREATE INDEX draft_page ON drafts (draft_page);
+CREATE INDEX draft_title_namespace ON drafts (draft_title, draft_namespace);
 
 COMMIT;
