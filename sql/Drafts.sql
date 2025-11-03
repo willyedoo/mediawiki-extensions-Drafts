ece1c3c7c07310a48ba59496a116b455c1eef63b
wisemoon <willyedoo@gmail.com>
2025-11-03 16:59:42 +0100

    Migrate Drafts table to PostgreSQL

--- sql/Drafts.sql
+++ sql/Drafts.sql
@@ -1,39 +1,24 @@
--- 
--- SQL for Drafts Extension
--- 
--- Table for storing working changes to pages that
--- users have yet to commit.
-CREATE TABLE /*_*/drafts (
-    -- Unique ID for drafts
-    draft_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    -- Unique value generated at edit time to prevent duplicate submissions
-    draft_token VARBINARY(255),
-    -- User who made the draft, 0 for anons
-    draft_user INTEGER NOT NULL default 0,
-    -- Page ID, 0 for proposed new pages
-    draft_page INTEGER NOT NULL default 0,
-    -- Original page namespace
-    draft_namespace INTEGER NOT NULL,
-    -- Original page db key
-    draft_title VARBINARY(255) NOT NULL default '',
-    -- Section ID of the article
-    draft_section INTEGER NULL,
-    -- Standard edit conflict checking params...
-    draft_starttime BINARY(14),
-    draft_edittime BINARY(14),
-    -- Timestamp when draft was saved...
-    draft_savetime BINARY(14),
-    -- Textarea scroll position saved from editor
-    draft_scrolltop INTEGER,
-    -- And of course the page and summary text come in handy
-    draft_text mediumblob NOT NULL,
-    draft_summary TINYBLOB,
-    -- Is this a minor edit?
-    draft_minoredit BOOL
-) /*$wgDBTableOptions*/;
+-- SQL for Drafts Extension (PostgreSQL version)
 
-CREATE INDEX /*i*/draft_user_savetime ON /*_*/drafts ( draft_user, draft_savetime );
-CREATE INDEX /*i*/draft_user_page_savetime ON /*_*/drafts ( draft_user, draft_page, draft_namespace, draft_title, draft_savetime );
-CREATE INDEX /*i*/draft_savetime ON /*_*/drafts (draft_savetime);
-CREATE INDEX /*i*/draft_page ON /*_*/drafts (draft_page);
-CREATE INDEX /*i*/draft_title ON /*_*/drafts (draft_title, draft_namespace);
+CREATE TABLE drafts (
+    draft_id SERIAL PRIMARY KEY,
+    draft_token BYTEA,
+    draft_user INTEGER NOT NULL DEFAULT 0,
+    draft_page INTEGER NOT NULL DEFAULT 0,
+    draft_namespace INTEGER NOT NULL,
+    draft_title BYTEA NOT NULL DEFAULT '',
+    draft_section INTEGER,
+    draft_starttime BYTEA,
+    draft_edittime BYTEA,
+    draft_savetime BYTEA,
+    draft_scrolltop INTEGER,
+    draft_text BYTEA NOT NULL,
+    draft_summary BYTEA,
+    draft_minoredit BOOLEAN
+);
+
+CREATE INDEX draft_user_savetime ON drafts (draft_user, draft_savetime);
+CREATE INDEX draft_user_page_savetime ON drafts (draft_user, draft_page, draft_namespace, draft_title, draft_savetime);
+CREATE INDEX draft_savetime ON drafts (draft_savetime);
+CREATE INDEX draft_page ON drafts (draft_page);
+CREATE INDEX draft_title ON drafts (draft_title, draft_namespace);
